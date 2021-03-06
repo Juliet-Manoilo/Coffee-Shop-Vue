0de6be783882213cc8d399b3dcae839febb7abb9
juliett manoilo <mnl.juliett@gmail.com>
2021-03-07 01:11:08 +0200

    :lipstick: map styles added

--- src/components/sections/GoogleMap.vue
+++ src/components/sections/GoogleMap.vue
@@ -5,7 +5,7 @@
       :zoom="zoom"
       :options="options"
       map-type-id="terrain"
-      style="width: 100%; height: 900px"
+      style="width: 100%; height: 700px"
     >
       <GmapMarker
         :key="index"
@@ -39,7 +39,7 @@ export default {
     zoomVal: {
       type: Number,
       default: function () {
-        return 15;
+        return 14;
       },
     },
   },
@@ -54,6 +54,239 @@ export default {
         streetViewControl: false,
         fullscreenControl: false,
         zoomControl: false,
+        styles: [
+          {
+            elementType: "geometry",
+            stylers: [
+              {
+                color: "#0f1114",
+              },
+            ],
+          },
+          {
+            elementType: "labels.text.fill",
+            stylers: [
+              {
+                color: "#787878",
+              },
+            ],
+          },
+          {
+            elementType: "labels.text.stroke",
+            stylers: [
+              {
+                color: "#18191b",
+              },
+            ],
+          },
+          {
+            featureType: "administrative.country",
+            elementType: "geometry.stroke",
+            stylers: [
+              {
+                color: "#4285f4",
+              },
+            ],
+          },
+          {
+            featureType: "administrative.land_parcel",
+            elementType: "labels.text.fill",
+            stylers: [
+              {
+                color: "#9e9e9e",
+              },
+            ],
+          },
+          {
+            featureType: "administrative.province",
+            elementType: "geometry.stroke",
+            stylers: [
+              {
+                color: "#4285f4",
+              },
+            ],
+          },
+          {
+            featureType: "landscape.man_made",
+            elementType: "geometry.stroke",
+            stylers: [
+              {
+                color: "#4285f4",
+              },
+            ],
+          },
+          {
+            featureType: "landscape.natural",
+            elementType: "geometry",
+            stylers: [
+              {
+                color: "#023e58",
+              },
+            ],
+          },
+          {
+            featureType: "poi",
+            elementType: "geometry",
+            stylers: [
+              {
+                color: "#283d6a",
+              },
+            ],
+          },
+          {
+            featureType: "poi",
+            elementType: "labels.text.fill",
+            stylers: [
+              {
+                color: "#8f8f8f",
+              },
+            ],
+          },
+          {
+            featureType: "poi",
+            elementType: "labels.text.stroke",
+            stylers: [
+              {
+                color: "#000000",
+              },
+            ],
+          },
+          {
+            featureType: "poi.park",
+            elementType: "geometry.fill",
+            stylers: [
+              {
+                color: "#101e1e",
+              },
+            ],
+          },
+          {
+            featureType: "poi.park",
+            elementType: "labels.text.fill",
+            stylers: [
+              {
+                color: "#3C7680",
+              },
+            ],
+          },
+          {
+            featureType: "road",
+            elementType: "geometry",
+            stylers: [
+              {
+                color: "#292f38",
+              },
+            ],
+          },
+          {
+            featureType: "road",
+            elementType: "labels.text.fill",
+            stylers: [
+              {
+                color: "#5d6674",
+              },
+            ],
+          },
+          {
+            featureType: "road",
+            elementType: "labels.text.stroke",
+            stylers: [
+              {
+                color: "#000000",
+              },
+            ],
+          },
+          {
+            featureType: "road.highway",
+            elementType: "geometry",
+            stylers: [
+              {
+                color: "#293b51",
+              },
+            ],
+          },
+          {
+            featureType: "road.highway",
+            elementType: "geometry.stroke",
+            stylers: [
+              {
+                color: "#0b0b0f",
+              },
+            ],
+          },
+          {
+            featureType: "road.highway",
+            elementType: "labels.text.fill",
+            stylers: [
+              {
+                color: "#b0d5ce",
+              },
+            ],
+          },
+          {
+            featureType: "road.highway",
+            elementType: "labels.text.stroke",
+            stylers: [
+              {
+                color: "#023e58",
+              },
+            ],
+          },
+          {
+            featureType: "transit",
+            elementType: "labels.text.fill",
+            stylers: [
+              {
+                color: "#98a5be",
+              },
+            ],
+          },
+          {
+            featureType: "transit",
+            elementType: "labels.text.stroke",
+            stylers: [
+              {
+                color: "#1d2c4d",
+              },
+            ],
+          },
+          {
+            featureType: "transit.line",
+            elementType: "geometry.fill",
+            stylers: [
+              {
+                color: "#283d6a",
+              },
+            ],
+          },
+          {
+            featureType: "transit.station",
+            elementType: "geometry",
+            stylers: [
+              {
+                color: "#3a4762",
+              },
+            ],
+          },
+          {
+            featureType: "water",
+            elementType: "geometry",
+            stylers: [
+              {
+                color: "#000000",
+              },
+            ],
+          },
+          {
+            featureType: "water",
+            elementType: "labels.text.fill",
+            stylers: [
+              {
+                color: "#354245",
+              },
+            ],
+          },
+        ],
       },
       mapTypeId: "styledMapType",
       locations: [
